9718b8a0e7f327689c5dcf41bfe4118246c5ec69
0H-Y3ah <136733009+0H-Y3ah@users.noreply.github.com>
2024-04-27 13:32:57 -0400

    Added main and others

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer>
+        <div id="footerText">
+            Copyright @2024 Premier Pokemon Rentals
+            <br>
+            139 Benson Avenue Sunnyshore City, Sinnoh Region
+        </div>
+</footer>
